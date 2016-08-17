a9f2909d74831b1fa3fc863b60212dd957f609a1
Peter Lewis <peter@peteralewis.co.uk>
2016-08-18 00:00:21 +0100

    Added menu function to controller to inject menu variables into all templates. Added display of menu to header - menu modifiable via Sections Table

--- src/FizzBuzz/Controller/AbstractController.php
+++ src/FizzBuzz/Controller/AbstractController.php
@@ -3,6 +3,7 @@
 namespace FizzBuzz\Controller;
 
 use FizzBuzz\Renderer;
+use FizzBuzz\Service\SectionsRepository;
 use Smrtr\SpawnPoint\AbstractController as SpawnAbstractController;
 
 abstract class AbstractController extends SpawnAbstractController
@@ -15,5 +16,23 @@ abstract class AbstractController extends SpawnAbstractController
     public function __construct()
     {
         $this->tpl = new Renderer;
+        $this->mainmenu();
+    }
+
+    private function mainmenu()
+    {
+        //var_dump($this->getRoutedParam);
+        $this->repoServices = new SectionsRepository();
+        $sections = $this->repoServices->findAll();
+        //$slug = $this->getRoutedParam('slug');
+
+        $menu = [];
+        foreach ($sections as $sectionDetails) {
+            if (!empty($sectionDetails['slug']) && !empty($sectionDetails['title'])) {
+                $menu[] = $sectionDetails;
+            }
+        }
+
+        $this->tpl->mainmenu = $menu;
     }
 }
